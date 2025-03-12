22314ff161e8395ac519b8140d89820386e282d0
yfussy <toainth@gmail.com>
2025-03-12 22:34:29 +0700

    feat(data): create Profile class

---
+++ data/profile-class.js
@@ -0,0 +1,13 @@
+class Profile {
+    age;
+    birthDate;
+    horoNum;
+    horoColor;
+
+    constructor(age, birthDate, horoNum, horoColor) {
+        this.age = age;
+        this.birthDate = birthDate;
+        this.horoNum = horoNum;
+        this.horoColor = horoColor;
+    }
+}
