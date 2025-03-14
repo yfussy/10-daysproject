498118098e311315cdca3bac9dce66cc416f80be
Sininant Wiriyakulnant <sininant@gmail.com>
2025-03-14 14:30:05 +0700

    fixing header

--- frontend/scripts/headerscript.js
+++ frontend/scripts/headerscript.js
@@ -1,13 +1,3 @@
-function press(button, buttonId) {
-    if (localStorage.getItem(buttonId) === 'true') {
-        button.classList.add('clicked');
-    }
-    button.addEventListener('click', function() {
-    
-        button.classList.add('clicked');
-        localStorage.setItem(buttonId, 'true');
-    });
-}
 
 const HomeButton = document.getElementById('Home-Button');
 press(HomeButton, 'Home-Button');  
@@ -17,3 +7,5 @@ const ClockButton = document.getElementById('Clock-Button');
 press(ClockButton, 'Clock-Button');  
 const HoroscopeButton = document.getElementById('Horoscope-Button');
 press(HoroscopeButton, 'Horoscope-Button');  
+
+
