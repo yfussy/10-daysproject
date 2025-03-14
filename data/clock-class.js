488a66ba870454b81288e9cd6414e7633d3d4c14
yfussy <toainth@gmail.com>
2025-03-14 16:08:54 +0700

    feat(data): add Clock class

---
+++ data/clock-class.js
@@ -0,0 +1,20 @@
+import { deltaTime } from "../frontend/scripts/utils/timeManager.js";
+
+class Clock {
+    day;
+    sleepTime;
+    sleepDuration;
+    wakeTime;
+    travelDuration;
+    appointmentTime;
+
+    constructor(day, sleepDuration, travelDuration, appointmentTime) {
+        this.day = day;
+        this.sleepDuration = sleepDuration;
+        this.travelDuration = travelDuration;
+        this.appointmentTime = appointmentTime;
+
+        this.wakeTime = deltaTime(appointmentTime, -travelDuration);
+        this.sleepTime = deltaTime(this.wakeTime, - sleepDuration);
+    }
+}
