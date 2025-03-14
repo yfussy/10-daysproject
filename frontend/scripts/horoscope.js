d156d626354b4a29cc59e73597ff8dace41a79e5
yfussy <toainth@gmail.com>
2025-03-14 22:41:40 +0700

    fix(random): random 2 color and adjust num range

--- frontend/scripts/horoscope.js
+++ frontend/scripts/horoscope.js
@@ -1,4 +1,16 @@
-function randomMongkolColor() {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    return `#${randomColor.padStart(6, '0')}`;
+function randomColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`
+}
+
+function randomMongkolKalakini() {
+    const randomMongkol = randomColor();
+    let randomKalakini = randomColor();
+    while (randomMongkol === randomKalakini) {
+        randomKalakini = randomColor();
+    }
+    return randomMongkol, randomKalakini; 
+}
+
+function randomMongkolNum() {
+    return Math.floor(Math.random() * 59);
 }
