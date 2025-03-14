e92b3b52002675692ac38ac0d9eb1201a89ec586
Sininant Wiriyakulnant <sininant@gmail.com>
2025-03-14 16:08:30 +0700

    header,images,mayaccount

--- frontend/scripts/headerscript.js
+++ frontend/scripts/headerscript.js
@@ -1,6 +1,5 @@
 
 const HomeButton = document.getElementById('Home-Button');
-press(HomeButton, 'Home-Button');  
 const CalendarButton = document.getElementById('Calendar-Button');
 press(CalendarButton, 'Calendar-Button');  
 const ClockButton = document.getElementById('Clock-Button');
