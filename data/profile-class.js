f9b6006a8058cce0f81bfffedb0fee2b5a51aaeb
yfussy <toainth@gmail.com>
2025-03-14 10:24:03 +0700

    feat(data): added loadProfile() (not complete)
    - get profile based on username login
    - url from backend

--- data/profile-class.js
+++ data/profile-class.js
@@ -11,3 +11,24 @@ class Profile {
         this.horoColor = horoColor;
     }
 }
+
+export function loadProfile(func) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', () => {
+        let profile = JSON.parse(xhr.response);
+        // get profile based on username login
+
+        console.log("Profile Loaded!");
+
+        func();
+    });
+
+    xhr.addEventListener('error', (error) => {
+        console.log('Unexpected error...');
+    });
+
+    xhr.open('GET', url);
+    // url api from backend
+    xhr.send();
+}
