0da41efbff2545301c6473834b2cc1caeac7bbbe
yfussy <toainth@gmail.com>
2025-03-14 21:55:28 +0700

    feat(script): add random number generator

--- frontend/scripts/horoscope.js
+++ frontend/scripts/horoscope.js
@@ -1,3 +1,4 @@
-function randomMongkolNum() {
-    
+function randomMongkolColor() {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    return `#${randomColor.padStart(6, '0')}`;
 }
