271d26b5e29a381298e518c9038529adeb269622
yfussy <toainth@gmail.com>
2025-03-15 12:48:23 +0700

    import userSchema and send to server

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const User = require('./models/user.model.js')
 require('dotenv').config();
 
 const app = express();
 
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("Hello from Node API Server");
+});
+
+app.post('/api/users', async (req, res) => {
+    try{
+        const user = await User.create(req.body);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
 });
 
 mongoose.connect(process.env.MONGO_URI)
