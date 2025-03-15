74aa0f91fd05c4a6cf8a700e26acba901570efa8
yfussy <toainth@gmail.com>
2025-03-15 12:47:20 +0700

    create userSchema

---
+++ backend/models/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema(
+    {
+        username:{
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+    },
+    {
+        Timestamp: true,
+    }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
