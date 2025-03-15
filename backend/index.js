663abee76281df485a7ad1383e23723002cc6668
yfussy <toainth@gmail.com>
2025-03-15 13:45:22 +0700

    feat(API): get all id and specified id

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,25 @@ app.get('/', (req, res) => {
     res.send("Hello from Node API Server");
 });
 
+app.get('/api/users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.get('/api/user/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
 app.post('/api/users', async (req, res) => {
     try{
         const user = await User.create(req.body);
