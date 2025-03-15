467f9cf57a0a9dc6c22cc5548f1ab6a017d3256d
yfussy <toainth@gmail.com>
2025-03-15 22:32:59 +0700

    feat(model): add clocklog model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,50 @@
+const { ReturnDocument } = require('mongodb');
 const mongoose = require('mongoose');
+
+const ClockSchema = mongoose.Schema(
+    {
+        date: {
+            type: String,
+            required: true
+        },
+        sleepTime: {
+            type: String,
+            required: true
+        },
+        sleepDuration: {
+            type: Number,
+            required: true
+        },
+        wakeTime: {
+            type: String,
+            required: true
+        },
+        travelDuration: {
+            type: Number,
+            required: true
+        },
+        appointmentTime: {
+            type: String,
+            required: true
+        },
+        fortune: {
+            color: {
+                colorFortune: {
+                    type: String,
+                    required: true
+                },
+                colorUnFortune: {
+                    type: String,
+                    required: true
+                }
+            },
+            number: {
+                type: Number,
+                required: true
+            }
+        }
+    }
+);
 
 const UserSchema = mongoose.Schema(
     {
@@ -10,6 +56,7 @@ const UserSchema = mongoose.Schema(
             type: String,
             required: true
         },
+        clockLogs: [ClockSchema]
     },
     {
         timestamps: true,
