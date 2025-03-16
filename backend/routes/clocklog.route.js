df0c6930edb845c8182ab07f998db7c2ee23fb66
yfussy <toainth@gmail.com>
2025-03-16 19:49:35 +0700

    feat(api): generating fortune w/ logics
    - once per day
    - multiple entry for clockLogs (fortune -> clock | clock -> fortune)

--- backend/routes/clocklog.route.js
+++ backend/routes/clocklog.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { addSleepLog, getClockLogByDate, getClockLogsByMonth } = require('../controllers/clocklog.controller');
+const { addSleepLog, getClockLogByDate, getClockLogsByMonth, generateOrUpdateFortuneForToday } = require('../controllers/clocklog.controller');
 
 const verifyToken = require('../middlewares/auth.middleware.js');
 
@@ -10,5 +10,7 @@ router.post('/', verifyToken, addSleepLog);
 router.get('/:date', verifyToken, getClockLogByDate);
 
 router.get('/month/:month', verifyToken, getClockLogsByMonth);
+
+router.patch('/fortune/', verifyToken, generateOrUpdateFortuneForToday)
 
 module.exports = router;
