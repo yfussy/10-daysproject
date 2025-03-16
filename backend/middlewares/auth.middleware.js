8668a96538783d4cf40099bd1cb16ce0a8bf95d3
yfussy <toainth@gmail.com>
2025-03-16 20:14:26 +0700

    perf(token): handles undefined authorization

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -2,7 +2,11 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1];
+        const authHeader = req.headers.authorization;
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).json({ message: 'No token provided' });
+        }
+        const token = authHeader.split(' ')[1];
 
         if (!token) {
             return res.status(403).json({ message: 'No token provided' });
