5cd8bbcd578844e23ccbe2e9d37ccd8ad475095e
yfussy <toainth@gmail.com>
2025-03-16 22:08:28 +0700

    feat(login): create login test page (w/ css,js)
    - login with user:admin, pass:admin for testing
    - get token from database

---
+++ frontend/scripts/loginscript.js
@@ -0,0 +1,33 @@
+const backURL = "http://localhost:3000";
+
+document.getElementById("login-form").addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    console.log(username, password);
+    
+
+    try {
+        const response = await fetch(`${backURL}/api/users/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            localStorage.setItem('token', data.token);
+            alert("Login successful!");
+            window.location.href = "testhoro.html";
+        } else {
+            alert(data.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert("An error occurred during login.");
+    }
+});
