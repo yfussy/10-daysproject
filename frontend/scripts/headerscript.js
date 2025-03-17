77494b1fb76816783756e05baa48423862da2202
yfussy <toainth@gmail.com>
2025-03-17 22:15:31 +0700

    feat(header): and logic to horoscope page
    - -> horoscope.hmtl: when not spin in that day
    - -> horoscope-result.html: when spun

--- frontend/scripts/headerscript.js
+++ frontend/scripts/headerscript.js
@@ -1,4 +1,3 @@
-
 const HomeButton = document.getElementById('Home-Button');
 const CalendarButton = document.getElementById('Calendar-Button');
 press(CalendarButton, 'Calendar-Button');  
@@ -7,4 +6,32 @@ press(ClockButton, 'Clock-Button');
 const HoroscopeButton = document.getElementById('Horoscope-Button');
 press(HoroscopeButton, 'Horoscope-Button');  
 
+HoroscopeButton.addEventListener('click', async() => {
+    const token = localStorage.getItem('token');
 
+    if (!token) {
+        alert('You must be logged in!');
+        return;
+    }
+
+    try {
+        const response = await fetch(`${backURL}/api/clocklogs/fortune/status`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        const { status } = await response.json();
+
+        if (status) {
+            window.location.href = "horoscope.html";
+        } else {
+            window.location.href = "horoscope-result.html";
+        }
+    } catch (error) {
+        console.error('Error getting fortune status:', error);
+        alert('Something went wrong! Check the console.');
+    }
+});
