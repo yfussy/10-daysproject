b89890bb93139b5f2b7d0ee42d1aea56d4e287f1
yfussy <167310929+yfussy@users.noreply.github.com>
2025-03-17 14:44:56 +0700

    add event structure to clocklogs + minor bug fixes (#38)
    * perf(fortune): simplify payload
    
    * fix(req): change request type
    
    * fix(date): update specific day
    
    * feat(event): create addEventByDate

--- backend/controllers/clocklog.controller.js
+++ backend/controllers/clocklog.controller.js
@@ -2,17 +2,17 @@ const User = require('../models/user.model.js');
 const { generateFortune, deltaTime} = require('./utils.js');
 
 // controllers
-// POST /api/clocklogs
-const addSleepLog = async (req, res) => {
-    const { sleepDuration, travelDuration, appointmentTime} = req.body;
+// PUT /api/clocklogs/:date
+const addSleepLogByDate = async (req, res) => {
+    const { sleepDuration, travelDuration, appointmentTime } = req.body;
     const userId = req.user.id;
+    const date = req.params;
 
     const wakeTime = deltaTime(appointmentTime, -travelDuration);
     const sleepTime = deltaTime(wakeTime, - sleepDuration);
 
     try {
         const user = await User.findById(userId);
-        const date = new Date().toISOString().split('T')[0]; // YYYY-MM-DD
         if (!user) {
             return res.status(404).json({message: "User not found"});
         }
@@ -76,7 +76,7 @@ const getClockLogsByMonth = async (req, res) => {
     }
 };
 
-// PATCH /api/clocklogs/fortune
+// PUT /api/clocklogs/fortune
 const generateOrUpdateFortuneForToday = async (req, res) => {
     const userId = req.user.id;
     const today = new Date().toISOString().split('T')[0]; // YYYY-MM-DD
@@ -98,12 +98,7 @@ const generateOrUpdateFortuneForToday = async (req, res) => {
         } else {
             user.clockLogs.push({
                 date: today,
-                sleepTime: null,
-                wakeTime: null,
-                sleepDuration: 0,
-                travelDuration: 0,
-                appointmentTime: null,
-                fortune: newFortune
+                fortune: newFortune,
             });
         }
 
@@ -118,9 +113,46 @@ const generateOrUpdateFortuneForToday = async (req, res) => {
     }
 };
 
+// PUT /api/clocklogs/event/:date
+const addEventByDate = async (req, res) => {
+    const { type, title, location, duration, note } = req.body;
+    const event = {
+        type,
+        title,
+        location,
+        duration,
+        note
+    };
+    const userId = req.user.id;
+    const date = req.params;
+
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({message: "User not found"});
+        }
+
+        const existingLog = user.clockLogs.find(log => log.date === date);
+        if (existingLog) {
+            existingLog.event = event;
+        } else {
+            user.clockLogs.push({
+                date,
+                event
+            });
+        }
+
+        await user.save();
+        res.status(200).json({message: "Event saved!", event});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+};
+
 module.exports = {
-    addSleepLog,
+    addSleepLogByDate,
     getClockLogByDate,
     getClockLogsByMonth,
-    generateOrUpdateFortuneForToday
+    generateOrUpdateFortuneForToday,
+    addEventByDate
 }
--- backend/routes/clocklog.route.js
+++ backend/routes/clocklog.route.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { addSleepLog, getClockLogByDate, getClockLogsByMonth, generateOrUpdateFortuneForToday } = require('../controllers/clocklog.controller');
+const { addSleepLogByDate , getClockLogByDate, getClockLogsByMonth, generateOrUpdateFortuneForToday, addEventByDate } = require('../controllers/clocklog.controller');
 
 const verifyToken = require('../middlewares/auth.middleware.js');
 
 
-router.post('/', verifyToken, addSleepLog);
+router.put('/:date', verifyToken, addSleepLogByDate);
 
 router.get('/:date', verifyToken, getClockLogByDate);
 
 router.get('/month/:month', verifyToken, getClockLogsByMonth);
 
-router.patch('/fortune', verifyToken, generateOrUpdateFortuneForToday)
+router.put('/fortune', verifyToken, generateOrUpdateFortuneForToday);
+
+router.put('event/:date', verifyToken, addEventByDate);
 
 module.exports = router;
